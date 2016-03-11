c34c20438ff3862d87083ad5224d848a521093fd
MurakamiKeach <m.keach.11@gmail.com>
2016-03-11 21:27:55 +0900

    Fix typos and appearance

--- _shiny/PowerAnalysis/app.R
+++ _shiny/PowerAnalysis/app.R
@@ -13,8 +13,8 @@ ui <-
       sidebarMenu(
         menuItem("Plot", tabName = "plot", icon = icon("line-chart")),
         menuItem("Description", tabName = "description", icon = icon("file-text-o")),
-        menuItem("Source code (Github)", icon = icon("github"),
-                 href = "https://github.com/KeachMurakami/KeachMurakami.github.io/blob/master/_shiny/PowerAnalysis/app.R"),
+        menuItem("How to use?", icon = icon("github"),
+                 href = "https://github.com/KeachMurakami/KeachMurakami.github.io/blob/master/_shiny/PowerAnalysis"),
         sliderInput("power", "(a) 検定力", 5, 100, 80),
         sliderInput("noise", "(b) 偶然誤差のSD", 0, 50, 10)
       )
@@ -26,30 +26,9 @@ ui <-
           h2("Dashboard tab content"),
           textInput("samplesize", "(c) サンプルサイズ (n) の上限値 (縦軸最大値):", 30),
           textInput("signal", "(d) 母平均の増加・減少率の上限値 (横軸最大値):", 50),
-          htmlOutput("plot"),
-          h4("概要"),
-          h5(
-            "対照区・処理区の母平均の差の検定 (t検定) を行う際のサンプルサイズ (いわゆるn) を決定するための事前の検定力分析を行います。
-            'Description'にとても簡単なものではありますが、用語の説明があります。", br()
+          htmlOutput("plot")
           ),
-          h4("使い方"),
-          h5(
-            "(a)で検定力を指定します。", br(),
-            "(b)で偶然誤差のSDを指定します。平均値100に対するSD (= 変動係数) を入力してください。", br(),
-            "(c)・(d)で図の表示範囲を設定します", br(),
-            "母平均の増加・減少率: 対照区の母平均 (µctrl = 100) に対する処理区の母平均 (µtrtm) の変化率 ( |(µtrtm - µctrl) / µctrl - 1| ) です。", br(),
-            "必要となるサンプルサイズ: 対照区・処理区でそれぞれ必要となるnです。", br()
-          ),
-          
-          h4("解釈の仕方"),
-          h5(HTML(paste0(
-            "図内の線にマウスオーバーすると2つの値が表示されます。",
-            tags$span(style="color:red", "上の値 (X)"), "は母平均の増加・減少率、", tags$span(style="color:red", "下の値 (Y)"), "は必要となるnです。"))),
-          
-          h5("パターン1: X以上の効果がある処理の検出を目標とする場合、効果を十分な確率 (指定した検定力) で評価するために必要なnはYです。"),
-          h5("パターン2: 指定した偶然誤差の下で、Y回反復した実験では、指定した検定力を維持して検出できるのは、X以上の効果がある処理のみです。")
-          ),
-        
+
       tabItem(tabName = "description",
         h3("検定力分析とは？"),
         h5(
